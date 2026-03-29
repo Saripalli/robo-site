37a28916170ec667463da196c923337f84acc051
Srinivas Saripalli <srini@Srinivass-MacBook-Pro.local>
2026-03-29 11:07:41 +0530

    Site index xml added

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,32 @@
+export default function Sitemap() {}
+
+export async function getServerSideProps({ res }) {
+  const baseUrl = "https://springlerobotics.com.au";
+
+  const pages = [
+    "",
+    "/catalog",
+    "/case-studies",
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+      .map((page) => {
+        return `
+          <url>
+            <loc>${baseUrl}${page}</loc>
+          </url>
+        `;
+      })
+      .join("")}
+  </urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
